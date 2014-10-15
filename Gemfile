3bde9a0be411a6f208d2a0d384f33148178a3366
fishermanswharff <fishermanswharff@github.com>
2014-10-14 21:09:21 -0400

    pulling tweets from Twitter api

--- Gemfile
+++ Gemfile
@@ -19,4 +19,3 @@ group :development, :test do
   gem 'web-console', '~> 2.0.0.beta4'
   gem 'spring'
 end
-
---
+++ app/controllers/tweets_controller.rb
@@ -0,0 +1,20 @@
+class TweetsController < ApplicationController
+
+  def index 
+    @client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["TWITTER_CONSUMER_KEY"]
+      config.consumer_secret     = ENV["TWITTER_CONSUMER_SECRET"]
+      config.access_token        = ENV["TWITTER_ACCESS_TOKEN"]
+      config.access_token_secret = ENV["TWITTER_ACCESS_SECRET"]
+    end
+
+    @jw_twitter = get_jw
+  end
+
+  def get_jw
+    @client.search("to:justinbieber marry me", :result_type => "recent").take(3).collect do |tweet|
+  "#{tweet.user.screen_name}: #{tweet.text}"
+end
+  end
+  
+end
